a6c1a083c6ac293771da49d2dc708e784b77b990
iamlucasgomes <lucas.devjs@gmail.com>
2023-04-28 18:26:28 -0300

    feat: implemented a function to test count_ocurrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,11 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    count_ocurrences_python = count_ocurrences("data/jobs.csv", "Python")
+    count_ocurrences_javascript = count_ocurrences(
+        "data/jobs.csv", "Javascript"
+    )
+
+    assert count_ocurrences_python == 1639
+    assert count_ocurrences_javascript == 122
