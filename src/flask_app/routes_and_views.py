d1ad07df0d5e573b78517f83e71ac4eadbd022a5
iamlucasgomes <lucas.devjs@gmail.com>
2023-04-30 20:23:56 -0300

    fix: corrected the route of the logo that did not render

--- src/flask_app/routes_and_views.py
+++ src/flask_app/routes_and_views.py
@@ -26,16 +26,16 @@ from src.flask_app.more_insights import (
 bp = Blueprint("client", __name__, template_folder="templates")
 
 
-@bp.route("/.images/job.png")
+@bp.route("/src/images/job.png")
 def flask_image():
-    return send_file("../../.images/job.png", mimetype="image/png")
+    return send_file("../../src/images/job.png", mimetype="image/png")
 
 
 @bp.route("/")
 def index():
     md = """
 <p align="center">
-    <img src="/.images/job.png" alt="Logo da Aplicação" width="800"/>
+    <img src="src/images/job.png" alt="Logo da Aplicação" width="800"/>
 </p>
 <h2 align="center">
     Boas-vindas ao Job Insights<br><br>
