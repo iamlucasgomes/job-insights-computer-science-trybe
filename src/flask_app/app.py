8e054144caa3875c90d3f6524a6ffccd8f415c5d
iamlucasgomes <lucas.devjs@gmail.com>
2023-05-02 18:08:13 -0300

    chore: hostname defined in docker-compose.yml

--- src/flask_app/app.py
+++ src/flask_app/app.py
@@ -4,10 +4,6 @@ from . import routes_and_views
 
 def create_app() -> Flask:
     app = Flask(__name__)
-
-    if __name__ == "__main__":
-        app.run(host="0.0.0.0", port=5000, debug=True)
-
     routes_and_views.init_app(app)
 
     return app
