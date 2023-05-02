9bd84dbb6999e1896233f5fcab60c5d03cab342d
iamlucasgomes <lucas.devjs@gmail.com>
2023-05-02 18:21:09 -0300

    chore: app.py back to original state

--- src/flask_app/app.py
+++ src/flask_app/app.py
@@ -5,8 +5,4 @@ from . import routes_and_views
 def create_app() -> Flask:
     app = Flask(__name__)
     routes_and_views.init_app(app)
-
-    if __name__ == "__main__":
-        app.run(host="0.0.0.0")
-
     return app
